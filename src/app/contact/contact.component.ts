dab314640b9ff471866a3a1ebd1e5df064cb97fa
Prabhat Singh <singhprabhat7.37@gmail.com>
2026-05-11 17:06:33 +0530

    Add files via upload

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
+
+@Component({
+  selector: 'app-contact',
+  imports: [SideNavComponent],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
