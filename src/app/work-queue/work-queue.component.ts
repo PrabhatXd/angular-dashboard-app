4e1f9aa3f81b8811aaafa7cf5064c144c0347903
Prabhat Singh <singhprabhat7.37@gmail.com>
2026-05-11 17:46:15 +0530

    Add files via upload

---
+++ src/app/work-queue/work-queue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
+@Component({
+  selector: 'app-work-queue',
+  imports: [SideNavComponent],
+  templateUrl: './work-queue.component.html',
+  styleUrl: './work-queue.component.scss'
+})
+export class WorkQueueComponent {
+
+}
