049bb1dd711fb7bd7d65789c30fd4080595e224e
Prabhat Singh <singhprabhat7.37@gmail.com>
2026-05-11 17:17:25 +0530

    Add files via upload

---
+++ src/app/help-page/help-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
+@Component({
+  selector: 'app-help-page',
+  imports: [SideNavComponent],
+  templateUrl: './help-page.component.html',
+  styleUrl: './help-page.component.scss'
+})
+export class HelpPageComponent {
+
+}
