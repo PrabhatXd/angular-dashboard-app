c5b634d4c62dd70032f73df126ea2b092b2fa578
Prabhat Singh <singhprabhat7.37@gmail.com>
2026-05-11 17:34:31 +0530

    Add files via upload

---
+++ src/app/statistics/statistics.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
+
+@Component({
+  selector: 'app-statistics',
+  imports: [SideNavComponent],
+  standalone: true,
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.scss'
+})
+export class StatisticsComponent {
+
+
+
+
+}
